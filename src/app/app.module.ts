a1a2e3770d2a467ec3ccda4f6ae0551e734ebdd9
alisson5g <alishotmelquiades@gmail.com>
2020-09-15 09:45:33 -0300

    Criando a estrutura de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoComponent } from './video-list/video/video.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoListComponent,
+    VideoComponent,
+    LeftMenuComponent
   ],
   imports: [
     BrowserModule
